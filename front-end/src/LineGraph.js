e4be8d2f21d217d8f04822b146d2294ff603600d
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-06-18 00:05:58 -0400

    Figuring out how to display salary data

--- front-end/src/LineGraph.js
+++ front-end/src/LineGraph.js
@@ -8,20 +8,43 @@ const LineGraph = () => {
     const { displayedData, setDisplayedData, displayedPlan } = useContext(Context)
 
     const [zoomDomain, setZoomDomain] = useState()
+    const [graphData, setGraphData] = useState([
+        { x: new Date(2020, 12, 1), y: 270 },
+        { x: new Date(2020, 6, 1), y: 300 }
+    ])
 
     useEffect(() => {
         const fetchData = async () => {
             // Will have to change this when users exist
 
-            // const res = await fetch(`${apiBaseUrl}/plans/5eea33997d4f345b506cd65c`)
-            // const parsedRes = await res.json()
-            // const salary = parsedRes.salaries[0]
-            // const expenses = parsedRes.expenses
-            // console.log(salary)
-            // console.log(expenses)
+            const res = await fetch(`${apiBaseUrl}/plans/5eea33997d4f345b506cd65c`)
+            const parsedRes = await res.json()
+            const salaries = parsedRes.salaries
+            const expenses = parsedRes.expenses
+            console.log(salaries)
 
+            const salaryDisplayArray = salaries.map(salary => {
+                // Have to get number of months, then loop over each month probably with regular for loop
+                // (End year - start year) * 12 + (end month - start month)
+
+                return {
+                    // x: every month from start date to end date, y: (salary.afterTaxAmount / number of months) * (i + 1)
+                }
+            })
+
+            setGraphData(salaryDisplayArray)
+            // afterTaxAmount: 24000
+            // amountPerYear: 30000
+            // endDate: "2021-06-17T04:00:00.000Z"
+            // name: "Salary"
+            // planId: "5eea33997d4f345b506cd65c"
+            // startDate: "2020-07-17T04:00:00.000Z"
+            // taxRate: 0.2
+
+            console.log(expenses)
+            // setGraphData(salary.afterTaxAmount)
         }
-        // fetchData()
+        fetchData()
 
     }, [])
 
@@ -40,12 +63,9 @@ const LineGraph = () => {
                     style={{
                         data: { stroke: "tomato" }
                     }}
-                    data={[
-                        { a: new Date(2020, 12, 1), b: 270 },
-                        { a: new Date(2020, 6, 1), b: 300 }
-                    ]}
-                    x="a"
-                    y="b"
+                    data={graphData}
+                // x="a"
+                // y="b"
                 />
 
             </VictoryChart>
